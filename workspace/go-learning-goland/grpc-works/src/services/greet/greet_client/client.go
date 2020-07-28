4e8afb56226d28720ff0797e7db6c9cebf6b2aa7
Varun Gupta1 <varun.gupta1@SW-L-P00576.local>
2020-07-28 16:19:20 +0530

    added GRPC-work again

---
+++ workspace/go-learning-goland/grpc-works/src/services/greet/greet_client/client.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"services/greet/greetpb"
+	"log"
+)
+
+func main () {
+	fmt.Println("Hi i'm in client... ")
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("Sorry client cannot talk to server: %v: ", err)
+	}
+	defer conn.Close();
+
+	c := greetpb.NewGreetServiceClient(conn)
+
+	//callGreet(c);
+	callGreetFullName(c);
+}
+func  callGreetFullName(c greetpb.GreetServiceClient)  {
+	fmt.Println("in Call GreetFullName Function... ")
+
+	req:= &greetpb.GreetRequest {
+		Greeting: &greetpb.Greeting {
+			FistName :"Naveen",
+			LastName:"Kumar",
+		},
+	}
+
+	res, err := c.Greet(context.Background(), req)
+
+	if err!= nil {
+		log.Fatalf("Error While calleding GreetFullName : %v ", err)
+	}
+
+	log.Println("Response From the GreetFullNamec ", res.Result)
+}
+
+func  callGreet(c greetpb.GreetServiceClient)  {
+	fmt.Println("in Call Greet Function... ")
+
+	req:= &greetpb.GreetRequest {
+		Greeting: &greetpb.Greeting {
+			FistName :"Naveen",
+			LastName:"Kumar",
+		},
+	}
+
+	res, err := c.Greet(context.Background(), req)
+
+	if err!= nil {
+		log.Fatalf("Error While calleding Greet : %v ", err)
+	}
+
+	log.Println("Response From the Greet ", res.Result)
+}
